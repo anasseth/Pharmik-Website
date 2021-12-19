66452db2372bd60c1e9ecbd7db8e49f7785db6c0
mohammadafnan29 <mohammadafnan29@gmail.com>
2021-12-20 02:59:09 +0500

    UI fixesss add sucess msg pop up 19 dec 2021

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalManager } from 'ngb-modal';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  modalRef: any;
+  @ViewChild('myModal') myModal: any;
 
-  constructor() { }
+  constructor(private modalService: ModalManager,) { }
 
   ngOnInit(): void {
   }
-
+  openModal() {
+    this.modalRef = this.modalService.open(this.myModal, {
+      size: "md",
+      modalClass: 'mymodal',
+      hideCloseButton: false,
+      centered: false,
+      backdrop: true,
+      animation: true,
+      keyboard: false,
+      closeOnOutsideClick: true,
+      backdropClass: "modal-backdrop"
+    })
+  }
+  closeModal() {
+    this.modalService.close(this.modalRef);
+    //or this.modalRef.close();
+  }
 }
