b7acd46a27cf53dfbaa6564cac6c2d25d71d9e4a
Afnan <mohammad.afnan@millsoft.com.pk>
2022-02-09 20:14:46 +0500

    set bottom bar ui 9 feb

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { WebsiteInfoService } from 'src/app/Service/WebsiteInfo/webinfo.service';
 
 @Component({
@@ -9,10 +10,44 @@ import { WebsiteInfoService } from 'src/app/Service/WebsiteInfo/webinfo.service'
 export class FooterComponent implements OnInit {
 
   constructor(
-    public _WebsiteInfoService:WebsiteInfoService
+    public _WebsiteInfoService: WebsiteInfoService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  Goto() {
+    this.router.navigate(['/Aboutus']);
+
+  }
+
+  Goto1() {
+    this.router.navigate(['/Contactus']);
+
+  }
+
+  Goto2() {
+    this.router.navigate(['/Blog']);
+
+  }
+
+  Goto3() {
+    this.router.navigate(['/Shop']);
+
+  }
+
+  Goto4() {
+    this.router.navigate(['/Faqs']);
+
+  }
+
+  Goto5() {
+    this.router.navigate(['/Shoppingcart']);
+  }
+
+  Goto6() {
+    this.router.navigate(['']);
   }
 
 }
--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -27,29 +27,36 @@ export class LoginpageComponent implements OnInit {
 
   Goto() {
     this.router.navigate(['/Aboutus']);
-    console.log("afnan")
+    
   }
 
   Goto1() {
     this.router.navigate(['/Contactus']);
-    console.log("afnan")
+    
   }
 
   Goto2() {
     this.router.navigate(['/Blog']);
-    console.log("afnan")
+    
   }
 
   Goto3() {
     this.router.navigate(['/Shop']);
-    console.log("afnan")
+    
   }
 
   Goto4() {
     this.router.navigate(['/Faqs']);
-    console.log("afnan")
+    
   }
 
+  Goto5() {
+    this.router.navigate(['/Shoppingcart']);
+  }
+  
+  Goto6() {
+    this.router.navigate(['']);
+  }
   customOptions: OwlOptions = {
     loop: true,
     autoplay: true,
