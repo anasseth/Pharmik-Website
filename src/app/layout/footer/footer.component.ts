abcd9998f2b388345b2319bc44f6d4198c7c31b9
Afnan <mohammad.afnan@millsoft.com.pk>
2022-02-10 17:30:32 +0500

    set bottom bar ui 10 feb

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CategoriesService } from 'src/app/Service/Categories/categories.service';
 import { WebsiteInfoService } from 'src/app/Service/WebsiteInfo/webinfo.service';
 
 @Component({
@@ -8,13 +9,17 @@ import { WebsiteInfoService } from 'src/app/Service/WebsiteInfo/webinfo.service'
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  show:boolean= false;
   constructor(
     public _WebsiteInfoService: WebsiteInfoService,
+    public _CategoryService: CategoriesService,
+
     private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this._CategoryService.loadAllCategories();
+
   }
 
   Goto() {
@@ -49,5 +54,7 @@ export class FooterComponent implements OnInit {
   Goto6() {
     this.router.navigate(['']);
   }
+
+  
 
 }
